81dc0f603e4e5a290b016d6b9eb33300961599f5
ImPaha <pashashabashoff@gmail.com>
2020-08-24 16:47:30 +0700

    kk
    ///

---
+++ fookhork/Assets/GetItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetItem : MonoBehaviour
+{
+   public Text scoreview;
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreview.text = score.ToString();
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Hero")
+        {
+
+            PlayerPrefs.SetInt("score", PlayerPrefs.GetInt("score") + 1);
+            Destroy(gameObject);
+            
+        }
+    }
+
+}
